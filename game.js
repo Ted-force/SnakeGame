e61f24a26943a6acd91c89d19205d7570b5a08cc
Ted-force <81739199+Ted-force@users.noreply.github.com>
2021-05-04 18:22:10 +0530

    Update game.js

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ let gameOver = false;
 function main(currentTime) {
   if(gameOver) {
     if(confirm('Press ok to restart')) {
-      window.location.assign('https://ted-force.github.io/SnakeGame/');
+      window.location.assign('SnakeGame/');
     }
     return 
   }
