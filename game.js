129b3a9ec14d3e8a58b76cc92a71caad1ca02637
Piyush Rangari <81739199+Ted-force@users.noreply.github.com>
2021-07-18 18:56:29 +0530

    Update game.js

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ let gameOver = false;
 function main(currentTime) {
   if(gameOver) {
     if(confirm('Press ok to restart')) {
-      window.location.assign('/');
+      window.location.assign('');
     }
     return 
   }
